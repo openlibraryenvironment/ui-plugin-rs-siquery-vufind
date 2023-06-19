1b05c0478324b9db4deccca5751562c137bca612
Jason Skomorowski <jason@indexdata.com>
2023-06-19 18:30:12 -0400

    Search indices PR-1493

--- src/components/SearchAndFilter.js
+++ src/components/SearchAndFilter.js
@@ -1,15 +1,19 @@
 import { Button, Icon, SearchField } from '@folio/stripes/components';
 import { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const SearchAndFilter = ({ setSearchParams }) => {
+  const intl = useIntl();
   const [query, setQuery] = useState('');
+  const [searchIndex, setSearchIndex] = useState('AllFields');
+  const indices = ['AllFields', 'Title', 'Author', 'Subject', 'CallNumber', 'ISN', 'tag'];
   const handleQuery = q => setQuery(q?.target?.value);
   const handleSubmit = e => {
     e.preventDefault();
     e.stopPropagation();
     setSearchParams({
       lookfor: query,
+      type: searchIndex,
       field: ['id', 'authors', 'title', 'cleanIsbn', 'cleanIssn', 'cleanOclcNumber', 'edition', 'placesOfPublication', 'publicationDates', 'publishers']
     });
   };
@@ -21,6 +25,9 @@ const SearchAndFilter = ({ setSearchParams }) => {
         onChange={handleQuery}
         onClear={() => setQuery('')}
         value={query}
+        searchableIndexes={indices.map(i => ({ value: i, label: intl.formatMessage({ id:`ui-plugin-rs-siquery-vufind.index.${i}` }) }))}
+        selectedIndex={searchIndex}
+        onChangeIndex={e => setSearchIndex(e?.target?.value)}
       />
       <Button
         buttonStyle="primary"
